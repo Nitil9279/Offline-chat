82297c21e35e2a190ee7ec2ecbc86c127fcee201
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:13:18 +0530

    Add light and dark theme support in AppTheme

--- lib/untils/theme.dart
+++ lib/untils/theme.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static ThemeData light() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.blue,
+      brightness: Brightness.light,
+    );
+  }
+
+  static ThemeData dark() {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+    );
+  }
+}
