211ecf084e8b7416bb272f5f167d6a5efc1bbf84
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:47:38 +0530

    Delete lib/untils directory

--- lib/untils/constants.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  static const String appName = "Offline Chat";
-
-  static const int splashTime = 2;
-
-  static const double padding = 16;
-
-  static const double radius = 18;
-}
--- lib/untils/helpers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class Helpers {
-
-  static String formatTime(DateTime time) {
-    return DateFormat("hh:mm a").format(time);
-  }
-
-}
