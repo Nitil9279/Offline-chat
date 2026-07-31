52dbc5a12da9b6d3e1aecfbaddfc16538cc71caf
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:11:48 +0530

    Gggg

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,1 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
 
+class StorageService {
+  static const String userKey = "username";
+
+  Future<void> saveUserName(String name) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(userKey, name);
+  }
+
+  Future<String> getUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(userKey) ?? "User";
+  }
+}
