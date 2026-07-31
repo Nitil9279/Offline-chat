b9228436e8966bcedd44424a676481be61e7f15c
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:05:15 +0530

    Add ChatMessage model with properties

--- lib/models/chat_message.dart
+++ lib/models/chat_message.dart
@@ -1,1 +1,11 @@
+class ChatMessage {
+  final String text;
+  final bool mine;
+  final DateTime time;
 
+  ChatMessage({
+    required this.text,
+    required this.mine,
+    required this.time,
+  });
+}
