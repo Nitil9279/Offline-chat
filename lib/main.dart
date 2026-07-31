0c8e1dac43b6e2d94a37798dc9a2911a0a9f1358
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:04:23 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'services/nearby_service.dart';
+import 'screens/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => NearbyService(),
+        ),
+      ],
+      child: const OfflineChatApp(),
+    ),
+  );
+}
+
+class OfflineChatApp extends StatelessWidget {
+  const OfflineChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Offline Chat',
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+
+      darkTheme: ThemeData.dark(useMaterial3: true),
+
+      themeMode: ThemeMode.system,
+
+      home: const SplashScreen(),
+    );
+  }
+}
