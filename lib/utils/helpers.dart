13548fa8906c022c43dce8e93c89d5194592fe1e
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:53:21 +0530

    Hhgvh

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,1 +1,19 @@
+import 'package:intl/intl.dart';
 
+class Helpers {
+  static String formatTime(DateTime time) {
+    return DateFormat('hh:mm a').format(time);
+  }
+
+  static String formatDate(DateTime date) {
+    return DateFormat('dd MMM yyyy').format(date);
+  }
+
+  static bool isEmpty(String value) {
+    return value.trim().isEmpty;
+  }
+  static String capitalize(String text) {
+    if (text.isEmpty) return text;
+    return text[0].toUpperCase() + text.substring(1);
+  }
+}
