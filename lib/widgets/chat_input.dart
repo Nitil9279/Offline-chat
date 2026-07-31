97c051f2a78907469533068924decaded7eb93be
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:16:15 +0530

    Uuu

--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+class ChatInput extends StatelessWidget {
+  final TextEditingController controller;
+  final VoidCallback onSend;
+
+  const ChatInput({
+    super.key,
+    required this.controller,
+    required this.onSend,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  hintText: "Type message...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: FloatingActionButton(
+              mini: true,
+              onPressed: onSend,
+              child: const Icon(Icons.send),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
