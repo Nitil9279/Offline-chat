d8304b69a06515687055ec7dc3bec6ce29215af6
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:12:37 +0530

    Add AppConstants class with application constants

--- lib/untils/constants.dart
+++ lib/untils/constants.dart
@@ -1,1 +1,9 @@
+class AppConstants {
+  static const String appName = "Offline Chat";
 
+  static const int splashTime = 2;
+
+  static const double padding = 16;
+
+  static const double radius = 18;
+}
