ddf68f0377155c042b253cf35f97ee4f13f2d9aa
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:14:47 +0530

    Add DeviceTile widget for displaying devices

--- lib/widgets/device_tile.dart
+++ lib/widgets/device_tile.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import '../models/device_model.dart';
 
+class DeviceTile extends StatelessWidget {
+  final DeviceModel device;
+  final VoidCallback onConnect;
+
+  const DeviceTile({
+    super.key,
+    required this.device,
+    required this.onConnect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 6,
+      ),
+      child: ListTile(
+        leading: const CircleAvatar(
+          child: Icon(Icons.phone_android),
+        ),
+        title: Text(device.name),
+        subtitle: Text(device.endpointId),
+        trailing: ElevatedButton(
+          onPressed: onConnect,
+          child: const Text("Connect"),
+        ),
+      ),
+    );
+  }
+}
