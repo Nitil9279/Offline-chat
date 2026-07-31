7d7ba4dc814f4aabc69006f116a76673811b0c85
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:15:37 +0530

    Jj

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
+import '../models/chat_message.dart';
 
+class MessageBubble extends StatelessWidget {
+  final ChatMessage message;
+
+  const MessageBubble({
+    super.key,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: message.mine
+          ? Alignment.centerRight
+          : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: message.mine
+              ? Colors.blue
+              : Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: Text(
+          message.text,
+          style: TextStyle(
+            color: message.mine
+                ? Colors.white
+                : Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
