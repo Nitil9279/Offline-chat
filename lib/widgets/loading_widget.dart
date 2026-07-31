f2e1708c95e82b60c25a11243be70849c4b2b22d
Nitil9279 <nitilk518@gmail.com>
2026-07-31 23:19:14 +0530

    Huuh

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class LoadingWidget extends StatelessWidget {
+  final String text;
+
+  const LoadingWidget({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(),
+          const SizedBox(height: 15),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
